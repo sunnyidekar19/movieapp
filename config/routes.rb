6bc1d7f6dadea946993c2468f2b84d16d28fd716
Sunny Idekar <sunnyidekar19@gmail.com>
2017-04-20 22:24:43 +0000

    updated config file

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   
   get '/users/info', to: 'users#info'
   post '/users/charge', to: 'users#charge'
-  get '/users/cancel_subscription', to: 'users#cancel_subscription  '
+  get '/users/cancel_subscription', to: 'users#cancel_subscription'
 
   resources :movies
   # The priority is based upon order of creation: first created -> highest priority.
